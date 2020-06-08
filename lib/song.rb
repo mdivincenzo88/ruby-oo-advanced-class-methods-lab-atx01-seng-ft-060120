8dd899faa3d9ad3412d435e61cdb2b3775e7db55
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-06-08 20:10:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,9 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+def initialize(name)
+  @name = name
+end
   def self.all
     @@all
   end
@@ -9,5 +12,10 @@ class Song
   def save
     self.class.all << self
   end
+  
+def create 
+      @@all < self.name
+      @name
+end
 
 end
