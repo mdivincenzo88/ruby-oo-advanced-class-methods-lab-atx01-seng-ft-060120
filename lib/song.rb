ae841dee46e0aac885703c0d42257d3de9186fdd
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-06-08 20:32:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,14 +18,15 @@ def self.create
       end
       
 def self.new_by_name (name)
-  
-  @song.name = name
-  @song
+  song = self.new
+  song.name = name
+  song
  end
 
 def self.create_by_name(name)
-  @song.name = name
- @@all << @song
+  song = self.new
+  song.name = name
+ @@all << song
   
 end
 
