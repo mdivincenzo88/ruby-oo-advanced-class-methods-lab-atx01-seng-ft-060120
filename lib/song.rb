d32a5161f80b1bcbcf27c228414d76994d6e164e
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-06-08 21:08:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,7 +54,7 @@ def self.new_from_filename (name)
   song
  end
  
- def self.create_from_filename
+ def self.create_from_filename(name)
     song= self.new
   song.name = (name.split(" - ")[1].chomp(".mp3"))
   song.artist_name = (name.split(" - ")[0])
